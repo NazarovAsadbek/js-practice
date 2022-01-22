b02102b1dcdc6638c5ac5365a925e7fb817d4e85
Asadbek Nazarov <nazarov_asadbek_job@mail.ru>
2022-01-22 14:32:52 +0500

    Lesson 026: Added functionality: 1) Show teacher modal v0.0.26 Spent Time: 30 minut

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -14,6 +14,20 @@ export default class Slider {
             this.slideIndex = this.slides.length;
         }
 
+        try {
+            this.hanson.style.opacity = '0';
+
+            if (n === 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
+            } else {
+                this.hanson.classList.remove('slideInUp');
+            }
+        } catch (e) {}
+
         this.slides.forEach(slide => {
             slide.style.display = 'none';
         });
@@ -26,6 +40,12 @@ export default class Slider {
     }
 
     render() {
+        try {
+            this.hanson = document.querySelector('.hanson');
+        } catch (e) {
+        }
+
+
         this.btns.forEach(item => {
             item.addEventListener('click', () => {
                 this.plusSlides(1);
