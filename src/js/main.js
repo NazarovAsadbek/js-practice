266a057b13f778b665937f93bb8b0b2acf48eca1
Asadbek Nazarov <nazarov_asadbek_job@mail.ru>
2022-01-23 17:28:59 +0500

    Lesson 30: Added functionality: 1) Difference in education block v0.0.30 Spent Time: 3 hours

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import MainSlider from "./modules/slider/slider-main";
 import VideoPlayer from "./modules/playVideo";
 import MiniSlider from "./modules/slider/slider-mini";
+import Difference from "./modules/difference";
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({container: '.page', btns: '.next'});
@@ -41,4 +42,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const player = new VideoPlayer('.showup .play', '.overlay');
     player.init();
+
+    new Difference('.officerold', '.officernew', '.officer__card-item').init();
 });
