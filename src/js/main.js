fd74312207245f14d28adadc1d896e54e1ed3288
Asadbek Nazarov <nazarov_asadbek_job@mail.ru>
2022-01-23 17:47:46 +0500

    Lesson 31: Added functionality: 1) Forms submit v0.0.31 Spent Time: 1 hour

--- src/js/main.js
+++ src/js/main.js
@@ -44,4 +44,5 @@ window.addEventListener('DOMContentLoaded', () => {
     player.init();
 
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
+    new Form('.form').init();
 });
