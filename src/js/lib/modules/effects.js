a6e3ed44473623e2a84c8961316da5454057ef3f
Asadbek Nazarov <nazarov_asadbek_job@mail.ru>
2022-01-27 10:35:51 +0500

    Lesson 043: Added functionality: 1) fade toggle v0.0.43 Spent Time: 20 minuts

--- src/js/lib/modules/effects.js
+++ src/js/lib/modules/effects.js
@@ -56,3 +56,30 @@ $.prototype.fadeOut = function (dur, fin) {
 
     return this;
 };
+
+$.prototype.fadeToggle = function (dur, display, fin) {
+    for (let i = 0; i < this.length; i++) {
+        if (window.getComputedStyle(this[i]).display === 'none') {
+            this[i].style.display = display || 'block';
+
+            const _fadeIn = (complection) => {
+                this[i].style.opacity = complection;
+            };
+
+            const ani = this.animateOverTime(dur, _fadeIn, fin);
+            requestAnimationFrame(ani)
+        } else {
+            const _fadeOut = (complection) => {
+                this[i].style.opacity = 1 - complection;
+                if (complection === 1) {
+                    this[i].style.display = 'none';
+                }
+            };
+
+            const ani = this.animateOverTime(dur, _fadeOut, fin);
+            requestAnimationFrame(ani)
+        }
+    }
+
+    return this;
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 import './lib/lib'
 
 $('#first').on('click', () => {
-    $('div').eq(1).fadeOut(800);
+    $('div').eq(1).fadeToggle(800);
 });
 $('[data-count="second"]').on('click', () => {
-    $('div').eq(2).fadeOut(800);
+    $('div').eq(2).fadeToggle(800);
 });
 $('button').eq(2).on('click', () => {
-    $('.w-500').fadeOut(800);
+    $('.w-500').fadeToggle(800);
 });
